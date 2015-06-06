d5f8e7ddf80af8f95fca1c9b74c7b45c5813aea9
1UnboundedSentience <jsnx21@gmail.com>
2015-06-06 12:06:18 -0700

    messy default control panel

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,14 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  
+
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-  
+
   // Form data for the login modal
   $scope.loginData = {};
 
@@ -42,14 +42,20 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PlaylistsCtrl', function($scope) {
+  /*$scope.playlists = [
+    { title: 'Match me now', id: 1 },
+    { title: 'Chat with Recipients', id: 2 },
+    { title: 'Account Status', id: 3 },
+    { title: 'Search for Recipients', id: 4 },
+    { title: 'Payment Info', id: 5 },
+    { title: 'Donate to 27%ers', id: 6 }
+  ];*/
+
   $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+    { title: 'About Us', id: 1 },
+
   ];
+
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
