4ac115334f5d654125183735dc944cc175c5cf99
Holman Gao <holman@golmansax.com>
2015-06-06 12:14:30 -0700

    Login page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, firebaseUrl) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -31,6 +31,18 @@ angular.module('starter.controllers', [])
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
+    var ref = new Firebase(firebaseUrl);
+    ref.authWithPassword({
+      email: $scope.loginData.username,
+      password: $scope.loginData.password
+    }, function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+      }
+    });
+
     console.log('Doing login', $scope.loginData);
 
     // Simulate a login delay. Remove this and replace with your login
