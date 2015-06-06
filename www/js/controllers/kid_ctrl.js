e6fdce723fa86142036a43e2c2d7093f0085d214
Holman Gao <holman@golmansax.com>
2015-06-06 16:35:24 -0700

    Add confirm dialog to kid page

--- www/js/controllers/kid_ctrl.js
+++ www/js/controllers/kid_ctrl.js
@@ -1,8 +1,21 @@
 angular.module('starter.controllers')
-.controller('KidCtrl', function($scope, $stateParams, firebaseUrl, $firebaseObject, $firebaseArray) {
+.controller('KidCtrl', function($scope, $stateParams, firebaseUrl, $firebaseObject,
+                                $firebaseArray, $state, $ionicPopup) {
   var ref = new Firebase(firebaseUrl);
   var kid = $firebaseObject(ref.child('kids').child($stateParams.kidId));
   $scope.kid = kid;
 
   $scope.needs = $firebaseArray(ref.child('needs').orderByChild('kid').equalTo($stateParams.kidId));
+
+  $scope.contribute = function (needId) {
+    var confirmPopup = $ionicPopup.confirm({
+      title: 'Contribute',
+      template: 'Your credit card ending with 4832 will be charged.  Continue?'
+    });
+    confirmPopup.then(function(res) {
+      if (res) {
+        $state.go('app.donations_success', { needId: needId });
+      }
+    });
+  };
 });
