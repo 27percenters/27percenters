e250f114e34d1bb21fbe9e8b2502f453abd3435b
Holman Gao <holman@golmansax.com>
2015-06-06 12:56:42 -0700

    Display needs

--- www/js/controllers/kid_ctrl.js
+++ www/js/controllers/kid_ctrl.js
@@ -2,4 +2,5 @@ angular.module('starter.controllers')
 .controller('KidCtrl', function($scope, $stateParams, firebaseUrl, $firebaseObject) {
   var ref = new Firebase(firebaseUrl + '/kids/' +  $stateParams.kidId);
   $scope.kid = $firebaseObject(ref);
+  $scope.needs = [{ name: 'Hello' }];
 });
